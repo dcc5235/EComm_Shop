747eaea9e0fd0d0249481c5223dfb801b6d780e7
Dany <dany@Danys-MacBook-Pro.local>
2020-11-17 11:32:14 -0600

    add JSON indentation for better formatting in json file

--- repositories/users.js
+++ repositories/users.js
@@ -29,9 +29,13 @@ class UsersRepository {
   async create(attrs) {
     const records = await this.getAll();
     records.push(attrs);
-    // write the updated 'records' array back to this.filename
-    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+
+    await this.writeAll(records);
   } 
+
+  async writeAll(records) {
+    await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+  }
 }
 
 const test = async () => {
