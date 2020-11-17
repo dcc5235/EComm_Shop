747df933ec7b8697d70cf9e2d30561f29bb57dbf
Dany <dany@Danys-MacBook-Pro.local>
2020-11-17 12:45:01 -0600

    updating records

--- repositories/users.js
+++ repositories/users.js
@@ -54,12 +54,24 @@ class UsersRepository {
     const filteredRecords = records.filter(record => record.id !== id);
     await this.writeAll(filteredRecords);
   }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find(record => record.id === id);
+
+    if (!record) {
+      throw new Error(`record with id ${id} not found`);
+    }
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
 
-  await repo.delete('e29476ec');
+  await repo.update('94dfcca5', { password: 'mypassword'});
 
 };
 
