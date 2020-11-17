dd1a89b239d985224f6e3fd1d85cbe1a48f401c0
Dany <dany@Danys-MacBook-Pro.local>
2020-11-16 18:44:07 -0600

    adding html form to js file

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hi there');
+const express = require('express');
+
+const app = express();
+
+// Route Handler
+app.get('/', (req, res) => {
+  res.send(`
+    <div>
+    <form>
+      <input placeholder="email" />
+      <input placeholder="password" />
+      <input placeholder="password confirmation" />
+      <button> Sign Up</button>
+    </form>
+    </div>
+    `); 
+});
+
+app.listen(3000, () => {
+  console.log('Listening');
+});
