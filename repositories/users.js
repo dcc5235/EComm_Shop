0bc64f778e96b5720aa4a084c9e88260b6265da7
Dany <dany@Danys-MacBook-Pro.local>
2020-11-17 11:22:36 -0600

    saving user records

--- repositories/users.js
+++ repositories/users.js
@@ -19,12 +19,25 @@ class UsersRepository {
     // read its contents
     // parse the contents
     // return the parsed data
-    return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8'}));
+    return JSON.parse
+      (await fs.promises.readFile(this.filename, { 
+        encoding: 'utf8'
+      })
+    );
   }
+
+  async create(attrs) {
+    const records = await this.getAll();
+    records.push(attrs);
+    // write the updated 'records' array back to this.filename
+    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+  } 
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
+
+  await repo.create({ email: 'test@test.com', password: 'password'});
 
   const users = await repo.getAll();
 
