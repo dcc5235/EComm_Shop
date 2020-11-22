979211576e08c8ea8e9c884ff4ef687be9950448
Dany <dany@Danys-MacBook-Pro.local>
2020-11-22 11:09:22 -0600

    product creation form

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-const productsRouter = require('.routes/admin/products');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
@@ -10,8 +10,8 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
-  keys: ['randomString']
-})
+    keys: ['lkasld235j']
+  })
 );
 app.use(authRouter);
 app.use(productsRouter);
