c663e69f9ba78e75dd399fda2123ee1b50f2e62a
Dany <dany@Danys-MacBook-Pro.local>
2020-11-23 13:42:05 -0600

    refactor

--- views/admin/products/index.js
+++ views/admin/products/index.js
@@ -2,13 +2,14 @@
 const layout = require('../layout');
 
 module.exports = ({ products }) => {
-  const renderedProducts = products.map((product) => {
-    return `
+  const renderedProducts = products
+    .map(product => {
+      return `
       <div>${product.title}</div>
     `;
     })
     .join('');
-  
+
   return layout({
     content: `
       <h1 class="title">Products</h1>
