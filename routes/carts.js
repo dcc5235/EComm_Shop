2c8b5921c57574daca9c888f03df221ea5ceff96
Dany <dany@Danys-MacBook-Pro.local>
2020-11-24 14:58:05 -0600

    request handler set up

--- routes/carts.js
+++ routes/carts.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+// receive a post request to add item to cart
+
+// rcv a GET request to show all items in cart
+
+// rcv post request to delete item from cart
+
 module.exports = router;
