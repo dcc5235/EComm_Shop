48b29dbd475b2f40f0470009fac1688033588cce
Dany <dany@Danys-MacBook-Pro.local>
2020-11-24 17:14:15 -0600

    func if no cart, create one and store cartId on req.session.cartId prop

--- routes/carts.js
+++ routes/carts.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const cartsRepo = require('../carts');
 
 const router = express.Router();
 
 router.post('/cart/products', (req, res) => {
   console.log(req.body.productId);
 
+  if (!req.session.cartId) {
+    const cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+
+  }
+
   res.send('Product added to cart');
 });
 
 // rcv a GET request to show all items in cart
+
 
 // rcv post request to delete item from cart
 
