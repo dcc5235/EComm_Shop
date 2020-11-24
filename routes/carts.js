b233fe5f094b7b257e1591997a5bba6ef4ed57fe
Dany <dany@Danys-MacBook-Pro.local>
2020-11-24 17:17:20 -0600

    fixed typo

--- routes/carts.js
+++ routes/carts.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const cartsRepo = require('../carts');
+const cartsRepo = require('../repositories/carts');
 
 const router = express.Router();
 
 router.post('/cart/products', (req, res) => {
   console.log(req.body.productId);
-
+  
+  let cart;
   if (!req.session.cartId) {
     const cart = await cartsRepo.create({ items: [] });
     req.session.cartId = cart.id;
   } else {
-    const cart = await cartsRepo.getOne(req.session.cartId);
+    cart = await cartsRepo.getOne(req.session.cartId);
   }
 
+  console.log(cart);
   res.send('Product added to cart');
 });
 
