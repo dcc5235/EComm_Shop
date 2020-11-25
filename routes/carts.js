1e4fdc99ebfd1984096c8b471a1e88f84be0f876
Dany <dany@Danys-MacBook-Pro.local>
2020-11-24 18:35:48 -0600

    rcv post request to delete item from cart

--- routes/carts.js
+++ routes/carts.js
@@ -46,6 +46,8 @@ router.get('/cart', async (req, res) => {
   res.send(cartShowTemplate({ items: cart.items }));
 });
 
-// rcv post request to delete item from cart
+router.post('/cart/products/delete', async (req, res) => {
+  console.log(req.body.itemId);
+});
 
 module.exports = router;
